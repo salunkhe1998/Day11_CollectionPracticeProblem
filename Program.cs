2fb530d3c9366d8b74255cabff68a3268739445e
salunkhe1998 <117974213+salunkhe1998@users.noreply.github.com>
2023-05-01 08:48:52 +0530

    find  max sell price markft

--- Program.cs
+++ Program.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace Day11_CollectionPracticeProblem
 {
-    class Program
+   class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day 11 collection practice Problem ");
             Console.WriteLine("Select any one option for Respective task\n" +
-                "1. Count Of Remaining Chapters");
+                "1. Count Of Remaining Chapters\n" +
+                "2. Max Selling Price ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -21,10 +22,15 @@ namespace Day11_CollectionPracticeProblem
                     chaptersCount.RemainingChapters();
                     break;
 
+                case 2:
+                    MaxSellPrice maxSellPrice = new MaxSellPrice();
+                    maxSellPrice.MaxSell();
+                    break;
+
                 default:
+                    Console.WriteLine("Please Enter valid option");
                     break;
             }
 
         }
     }
-}
