5d21739abbb35f7dd5519b7d7d36b41ea5a68607
Ashwini <salunkheashwini1998@gmail.com>
2023-05-01 08:29:46 +0530

    method calling

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_CollectionPracticeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 11 collection practice Problem ");
+            Console.WriteLine("Select any one option for Respective task\n" +
+                "1. Count Of Remaining Chapters");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    ChaptersCount chaptersCount = new ChaptersCount();
+                    chaptersCount.RemainingChapters();
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+    }
+}
